916a4118734b07dd1efeb0377e440a44702f45a7
kanggawe <m.ibrahim.bajuri@gmail.com>
2024-05-24 19:08:53 +0700

    rubah

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,41 @@
-
-import Button from '@mui/material/Button';
-import './App.css'
+//import Button from "@mui/material/Button";
+import "./App.css";
+//import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-   return (
-  <>
-  <div id="menutop-wrapper">
-      <div className="topmenu section" id="topmenu">
-        <nav id="menutop">
-          <input type="checkbox" />
-          <label htmlFor="toggleMenu"><span>&nbsp;</span></label>
-          <ul>
-            <li>
-            {/* <Link to="/">Home</Link> */}
-            <a href="/p/about.html" title="Tentang">About</a></li>
-            <li><a href="/p/kontak.html" title="Kontak">Kontak</a></li>
-            <li>
-              <a href="/p/disclaimer.html" title="Disclaimer">Disclaimer</a>
-            </li>
-            <li>
-              <a href="/p/sitemap.html" title="Daftar Isi Blog">Sitemap</a>
-            </li>
-            <li>
-              <a href="/p/pasang-iklan.html" title="Pasang Iklan">Pasang Iklan</a>
-            </li>
-            <li>
-              <a href="https://www.blogger.com/follow-blog.g?blogID=6192590825799115879" target="_blank" title="Follow CB Blogger">
-                <i className="fa fa-angle-double-right"></i> Follow DuniaKompi
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+  return (
+    <>
+      <Router>
+        <div id="menutop-wrapper">
+          <div className="topmenu section" id="topmenu">
+            <nav id="menutop">
+              <input type="checkbox" />
+              <label htmlFor="toggleMenu">
+                <span>&nbsp;</span>
+              </label>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
       <h1>Eling Semesta Alam</h1>
-      <div className="card">
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      </div>
-  </>
-
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/" component={Home} />
-    //     <Route path="/about" component={About} />
-    //     <Route path="/contact" component={Contact} />
-    //   </Switch>
-    // </Router>
-  
-  )
+      <div className="card"></div>
+    </>
+  );
 }
 
-export default App
+export default App;
