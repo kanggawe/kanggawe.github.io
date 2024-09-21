b3da5a4903c8752f7db5df23c7341f8c4848d5be
kanggawe <m.ibrahim.bajuri@gmail.com>
2024-09-21 15:36:20 +0700

    tambah class container

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Contact() {
-  return <h1>contact Page</h1>;
+  return (
+    <div className="container">
+      <h1>contact page</h1>
+    </div>
+  )
 }
 
 export default Contact;
