916ed1a88b68ead22972fd807d6572728aa6d649
kanggawe <m.ibrahim.bajuri@gmail.com>
2025-08-19 03:28:01 +0700

    Configure for root domain deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Sesuaikan dengan nama repository Anda
+  base: '/', // Untuk deploy di root domain
 })
