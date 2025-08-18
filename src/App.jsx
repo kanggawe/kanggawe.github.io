08280b43c3aa147bba8bb2aebc1972bf3c906794
kanggawe <m.ibrahim.bajuri@gmail.com>
2025-08-19 06:08:57 +0700

    Remove hash from URLs with BrowserRouter and improved routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
